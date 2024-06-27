97ed540cb5515211f2303929cc8e5a6064ac292a
TheFlyingRat <joeyspp50@gmail.com>
2024-06-27 21:20:18 +1000

    Fixes, refactoring and comments

--- static/search.js
+++ static/search.js
@@ -1,68 +1,68 @@
-// Defines the const's by making equal to the html elements in the home html file.
-const fileNameResultElement = document.getElementById("name_of_file");
-const productivityCheckResultElement = document.getElementById("productive_day");
+// Get the elements from the DOM
+const fileNameResultElement            = document.getElementById("name_of_file");
+const productivityCheckResultElement   = document.getElementById("productive_day");
 const timeToCompleteTasksResultElement = document.getElementById("time_to_complete_tasks")
-const bodyResultElement = document.getElementById("body");
+const bodyResultElement                = document.getElementById("body");
 
 
-// Declares the submitSearch() function.
+// Runs when the search is made
 async function submitSearch(event) {
-
-    // Prevents the default function of event from happening.
+    // Prevent form from submitting as it normally would
     event.preventDefault() 
-    // Submits the search to the server.
-    data = new FormData(event.target);
 
+    // Get the form data, extract the search term, make new json object
     const formData = new FormData(event.target);
-
+    const formJson = Object.fromEntries(formData.entries());
+    const jsonData = JSON.stringify(formJson);
     let search = formData.get("search");
 
-    let regex = /^[a-zA-Z0-9!@#$%^&*()_+-=`~<>?:"{}';/.,]+$/
-
-    if (search == ""){
-        
-        console.log("Error!")
-        
-        alert("Missing input");
-        
+    // Existence check for the search term
+    if (search == "") {
+        alert("Missing input!");
         return false;
     }
 
-    if (regex.test(search)){
-        
-        console.log("Error!")
-        
-        alert("Invalid input");
-        
+    // Regex for the allowable characters
+    const regex = /^[a-zA-Z0-9!@#$%^&*()_+-=`~<>?:"{}';/.,]+$/
+    if (!regex.test(search)) {
+        alert("Bad search term!");
         return false;
     }
 
-    // Awaits a response from the server.
+    // Send the search to the server
     const response = await fetch("/home/search", {
-        method: "post",
-        body: data,
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: jsonData,
     })
 
-    // Grabs the result from the server and stores it in the results const.
+    // Switch on the API response
+    switch(response.status) {
+        case 200:
+            // Okay, proceed
+            break;
+        case 404:
+            alert("Cannot find that diary entry!");
+            return false
+        default:
+            alert("Error!")
+            return false;
+    }
+
+    // Grabs the result from the server and stores it in the results const
     const result = await response.json();
 
     // Brings the data back to the home html file to be used.
-    fileNameResultElement.innerHTML = result.name_of_file;
-    productivityCheckResultElement.innerHTML = result.productive_day
-    timeToCompleteTasksResultElement.innerHTML = result.time_to_complete_tasks
-    bodyResultElement.innerHTML = result.body;
+    fileNameResultElement.innerHTML             = result.name_of_file;
+    productivityCheckResultElement.innerHTML    = result.productive_day
+    timeToCompleteTasksResultElement.innerHTML  = result.time_to_complete_tasks
+    bodyResultElement.innerHTML                 = result.body;
 
-    // Sets the searchedFrom as x for easier use.
-    var x = document.getElementById("searchedForm");
+    // Get the searchedForm element from the DOM
+    const searchedForm = document.getElementById("searchedForm");
 
-    // Changes the display of the searchedForm.
-    if(x.style.display == "none") {
-        // If searchedForm is none it then changes it to block.
-        x.style.display = "block";
-    } 
-
-    // Otherwise just keep the display of the searchedForm as block. 
-    else {
-        x.style.display = "block";
-    }
+    // Make searchedForm visible
+    searchedForm.style.display = "block";
 }
